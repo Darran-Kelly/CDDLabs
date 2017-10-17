e7083b6d0e04bc3fe69b9b34501cf63e7a03e04a
Darran Kelly <Test@test.com>
2017-10-17 15:56:37 +0100

    got rid of unwanted files.

--- Lab4/Docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['lunchbreak',['lunchBreak',['../barrier_8cpp.html#a8ab7df6d74dcc96eed8d52fc7b8275d9',1,'barrier.cpp']]]
-];
--- Lab4/Docs/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['c',['c',['../barrier_8cpp.html#a4e1e0e72dd773439e333c84dd762a9c3',1,'barrier.cpp']]]
-];
